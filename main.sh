e69cf51ea4f6ba30db1d2756733f573e58970c3f
bgdar <akunzero975@gmail.com>
2025-03-11 15:49:27 +0700

    add

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 #function untuk menghandle data    yang di simpan ke folder
-#handleBackup (){
-  
-#}
+handleBackup (){
+  echo "hasil dari = $1"
+}
 
 echo "pilih folder yang akan di backup :"
 echo "gunakan perintah 'init' untuk inisialisai folder saat ini \n "
@@ -12,12 +12,15 @@ read path_folder
 #cek apakah input adalah folder
 if [[ ! -d "$path_folder" && "$path_folder" != "init"  ]]; then
   echo "not is folder"
+
   #cek apakah perintah adalah init
 elif [[ "$path_folder" == "init" ]]; then
   path="$PWD"
-  echo "path saat ini  => ,$path user $USER"
+  handleBackup $path
+  
 else
   echo "arah , $path_folder"
+  handleBackup $path_folder
 fi
 
 
